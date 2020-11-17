43776e84db8e9bd393e49c75d14658fc41afa853
zan <852150909@qq.com>
2020-11-17 20:12:59 +0800

    整数翻转

---
+++ leetcodeExcecise/src/main/java/leetcode/Test11.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Test11 {
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0;i<height.length;i++){
+            for(int j = 1;j<height.length;j++){
+                max =Math.max(Math.abs(i-j)*Math.min(height[i],height[j]),max);
+            }
+        }
+        return max;
+    }
+}
